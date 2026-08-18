00748559ec51875e3eae4ca61964e8143a449372
Kelvin Chin <chinkaiwen_35@outlook.com>
2026-08-19 00:35:26 +0800

    Add Argon2 support and implement basic login logic

--- src/controller/auth_controller.rs
+++ src/controller/auth_controller.rs
@@ -1,3 +1,7 @@
+use argon2::{
+    Argon2, PasswordHasher, PasswordVerifier,
+    password_hash::{SaltString, rand_core::OsRng},
+};
 use poem_openapi::{
     OpenApi,
     payload::{Json, PlainText},
@@ -11,8 +15,26 @@ pub struct AuthController;
 impl AuthController {
     #[oai(path = "/login", method = "post")]
     pub async fn login(&self, credential: Json<LoginRequestDto>) -> PlainText<String> {
-        // Implement your login logic here
-        log::info!("Login attempt with {:?}", credential);
-        PlainText("Login successful".into())
+        let password = credential.password.as_bytes();
+        let argon2 = Argon2::default();
+        let salt = SaltString::generate(&mut OsRng);
+        let hash = argon2.hash_password(password, &salt);
+        match hash {
+            Ok(hash) => {
+                let hash_str = hash.to_string();
+                let read_hash = argon2::PasswordHash::new(&hash_str);
+
+                let verified = argon2.verify_password(password, &read_hash.unwrap());
+                match verified {
+                    Ok(_) => log::info!("Password verified successfully"),
+                    Err(e) => log::error!("Failed to verify password: {}", e),
+                }
+                PlainText(hash_str)
+            }
+            Err(e) => {
+                log::error!("Failed to hash password: {}", e);
+                PlainText("Error".into())
+            }
+        }
     }
 }
